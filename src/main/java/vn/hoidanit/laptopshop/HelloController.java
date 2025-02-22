c1d980299c49ca307b38a3bfb512587f5a0a9e5d
maboyzz <nthuy311023@gmail.com>
2025-02-22 13:45:31 +0700

    hello world

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,18 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/")
+
+    public String index() {
+
+        return "Hello World NT Huy1";
+
+    }
+
+}
